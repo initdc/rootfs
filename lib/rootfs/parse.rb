14d3202831201c1cb8542428026b1694b8b190f2
initdc <initd@outlook.com>
2023-01-04 14:28:43 +0000

    feat: add func to parse array in array
    Delete type conv of parse string func

--- lib/rootfs/parse.rb
+++ lib/rootfs/parse.rb
@@ -7,14 +7,36 @@ module RootFS
     def _str_in_arr(any, distro, attr, data)
       err_msg = "Valid #{distro} #{attr}: #{data}"
 
-      puts err_msg unless any
-
-      str = any.to_s
-      puts err_msg if str.empty?
-
-      return { "#{attr}": str } if data.include?(str)
+      return { "#{attr}": any } if data.include?(any)
 
       puts err_msg
+    end
+
+    def _arr_in_arr(any, distro, attr, data)
+      err_msg = "Valid #{distro} #{attr}: #{data}"
+
+      args = any.instance_of?(Array) ? any : [any]
+      if args.empty?
+        puts err_msg
+        return
+      end
+
+      res = []
+      not_valid = []
+      args.each do |arg|
+        if data.include?(arg)
+          res.push(arg)
+        else
+          not_valid.push(arg)
+        end
+      end
+
+      unless not_valid.empty?
+        puts "Not valid: #{not_valid}"
+        puts err_msg
+      end
+
+      { "#{attr}": res } unless res.empty?
     end
   end
 end
